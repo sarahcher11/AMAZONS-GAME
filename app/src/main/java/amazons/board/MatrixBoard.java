e650077dbc527e025625923d0bec0dfc5376964e
Sarah Cher <ls_cherchem@esi.dz>
2023-11-17 15:38:04 +0100

    Ajout de la classe MatrixBoard

---
+++ app/src/main/java/amazons/board/MatrixBoard.java
@@ -0,0 +1,50 @@
+package amazons.board;
+
+import amazons.figures.Figure;
+
+public class MatrixBoard implements Board{
+    /**
+     * Place the given figure {@code figure} at the position {@code position}. Nothing happens if {@code position}
+     * is outside this board.
+     *
+     * @param position : where the figure has to be positioned
+     * @param figure   : the figure to be placed at position {@code position}
+     */
+    @Override
+    public void setFigure(Position position, Figure figure) {
+
+    }
+
+    /**
+     * Returns the figure at the specified position in the board.
+     *
+     * @param position : position of the figure to return
+     * @return : the figure at the specified position in the board
+     */
+    @Override
+    public Figure getFigure(Position position) {
+        return null;
+    }
+
+    /**
+     * Return {@code true} if the specified position is empty (i.e., contains EMPTY_FIGURE)
+     *
+     * @param position : position to test
+     * @return {@code true} if the specified position is empty in the board.
+     */
+    @Override
+    public boolean isEmpty(Position position) {
+        return false;
+    }
+
+    /**
+     * Return {@code true} if the specified position is out of the board.
+     *
+     * @param position : position to test
+     * @return {@code true} if the specified position is out of the board
+     */
+    @Override
+    public boolean isOutOfBoard(Position position) {
+        return false;
+    }
+}
