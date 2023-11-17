7639d7061c73c39eb41de1b00e8c17c1ad5ff43f
m23024284 <ines.mederbel@etu.univ-amu.fr>
2023-11-17 14:58:17 +0100

    Ajout de la methode boolean equals

--- app/src/main/java/amazons/board/Position.java
+++ app/src/main/java/amazons/board/Position.java
@@ -52,9 +52,18 @@ public class Position implements Serializable {
     }
 
     @Override
-
     public String toString()
     {
         return "("+x+","+y+")";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+
 }
