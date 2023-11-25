6db0ac7f0b1d9358a9592efc3244336134cbf4b7
Sarah Cher <ls_cherchem@esi.dz>
2023-11-25 16:21:47 +0100

    Ajout du constructeur PresetFigureGenerator

--- app/src/main/java/amazons/board/PresetFigureGenerator.java
+++ app/src/main/java/amazons/board/PresetFigureGenerator.java
@@ -1,8 +1,17 @@
 package amazons.board;
 
 import amazons.figures.Figure;
+import amazons.figures.MovableFigure;
+
+import java.util.List;
 
 public class PresetFigureGenerator implements FigureGenerator{
+
+    private List<MovableFigure> figures;
+    public PresetFigureGenerator(List<MovableFigure> figures)
+    {
+            this.figures=figures;
+    }
     @Override
     public Figure nextFigure(Position position) {
         return null;
