5021a97a6a3c8cc16d276a1be86021b01d442a84
Sarah Cher <ls_cherchem@esi.dz>
2023-11-25 13:20:52 +0100

    Finalisation de la tache 6

--- app/src/main/java/amazons/figures/Figure.java
+++ app/src/main/java/amazons/figures/Figure.java
@@ -1,6 +1,5 @@
 package amazons.figures;
 
-import amazons.IllegalMoveException;
 import amazons.board.Board;
 import amazons.board.Position;
 import amazons.player.PlayerID;
@@ -23,7 +22,7 @@ public interface  Figure extends Serializable {
      * is illegal according to the rules of the game.
      * @param position: the position to which this should be moved
      */
-    void moveTo(Position position, Board board) throws amazons.figures.IllegalMoveException;
+    void moveTo(Position position, Board board) throws IllegalMoveException;
 
     /**
      * Set the position of this figure to the given position.
