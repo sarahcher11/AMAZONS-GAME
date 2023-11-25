4290b6c80425e50430b46ae70989516891671290
Sarah Cher <ls_cherchem@esi.dz>
2023-11-25 16:36:45 +0100

    Ajout du constructeur RandomFigureGenerator

--- app/src/main/java/amazons/board/RandomFigureGenerator.java
+++ app/src/main/java/amazons/board/RandomFigureGenerator.java
@@ -1,4 +1,23 @@
 package amazons.board;
 
+import amazons.figures.MovableFigure;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
 public class RandomFigureGenerator {
+
+
+    private Random random;
+   private List<MovableFigure> figures;
+    private  Iterator<Position> positionIterator;
+
+
+    public RandomFigureGenerator(Random random, List<MovableFigure> figures, Iterator<Position> positionIterator)
+    {
+         this.random=random;
+         this.figures=figures;
+         this.positionIterator=positionIterator;
+    }
 }
