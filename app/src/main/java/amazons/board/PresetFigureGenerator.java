63645cb96cefdce1246a47382c3cd405b4f4a066
Sarah Cher <ls_cherchem@esi.dz>
2023-11-25 16:18:30 +0100

    Ajout de la classe PresetFigureGenerator

---
+++ app/src/main/java/amazons/board/PresetFigureGenerator.java
@@ -0,0 +1,10 @@
+package amazons.board;
+
+import amazons.figures.Figure;
+
+public class PresetFigureGenerator implements FigureGenerator{
+    @Override
+    public Figure nextFigure(Position position) {
+        return null;
+    }
+}
