63cbf2bcb56e6cc468854974c00111498a08f449
Ines MEDERBEL <ls_cherchem@esi.dz>
2023-11-25 22:19:59 +0100

    Redéfinition de hashCode et equals

--- app/src/main/java/amazons/player/Move.java
+++ app/src/main/java/amazons/player/Move.java
@@ -13,30 +13,42 @@ public class Move {
     public static final Move DUMMY_MOVE = new Move();
 
 
-    // TODO complete the code of this class
+
+    public Move(Position amazonStartPosition, Position amazonDestPosition, Position arrowDestPosition) {
+
+        this.amazonStartPosition=amazonStartPosition;
+        this.amazonDstPosition=amazonDestPosition;
+        this.arrowDstPosition=arrowDestPosition;
+
+    }
+
+    public Move(){}
 
     public Position getAmazonStartPosition() {
         return amazonStartPosition;
-        // TODO
+
     }
 
     public Position getAmazonDstPosition() {
         return amazonDstPosition;
-        // TODO
+
     }
 
     public Position getArrowDestPosition() {
-        return null;
-        // TODO
+        return arrowDstPosition;
+
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Move move)) return false;
+        return Objects.equals(amazonStartPosition, move.amazonStartPosition) && Objects.equals(amazonDstPosition, move.amazonDstPosition) && Objects.equals(arrowDstPosition, move.arrowDstPosition);
+    }
 
-    private Move() {}
-
-
-    // TODO
-    public Move(Position amazonStartPosition, Position amazonDestPosition, Position arrowDestPosition) {
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(amazonStartPosition, amazonDstPosition, arrowDstPosition);
     }
 
 
