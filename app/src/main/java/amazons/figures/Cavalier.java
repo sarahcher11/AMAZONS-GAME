69cf4c373ef7275c4dd5c6b03f6cd346715b77fb
Sarah CHERCHEM <ls_cherchem@esi.dz>
2023-12-06 22:48:37 +0100

    Ajout de la classe Cavalier

---
+++ app/src/main/java/amazons/figures/Cavalier.java
@@ -0,0 +1,18 @@
+package amazons.figures;
+
+import amazons.board.Board;
+import amazons.board.Position;
+
+import java.util.List;
+
+public class Cavalier extends MovableFigure {
+    @Override
+    public List<Position> getAccessiblePositions(Board board) {
+        return null;
+    }
+
+    @Override
+    public List<Position> getPositionAdjacente(Board board) {
+        return null;
+    }
+}
