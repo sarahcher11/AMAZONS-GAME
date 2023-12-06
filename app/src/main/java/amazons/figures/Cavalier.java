9aafa27dc6e102223eedb5f09fc915368a9b0253
Sarah CHERCHEM <ls_cherchem@esi.dz>
2023-12-06 23:09:52 +0100

    Ajout de getAccesible de Cavalier

--- app/src/main/java/amazons/figures/Cavalier.java
+++ app/src/main/java/amazons/figures/Cavalier.java
@@ -1,14 +1,30 @@
 package amazons.figures;
 
 import amazons.board.Board;
+import amazons.board.CardinalDirection;
 import amazons.board.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cavalier extends MovableFigure {
     @Override
     public List<Position> getAccessiblePositions(Board board) {
-        return null;
+        List<Position> positions = new ArrayList<>();
+
+        for (CardinalDirection card : CardinalDirection.values()) {
+            int x = this.position.getX() + card.deltaColumn;
+            int y = this.position.getY() + card.deltaRow;
+
+            while (!board.isOutOfBoard(new Position(x,y)) ) {
+                if (board.isEmpty(new Position(x,y)))
+                  positions.add(new Position(x, y));
+                x += card.deltaColumn;
+                y += card.deltaRow;
+            }
+        }
+
+        return positions;
     }
 
     @Override
